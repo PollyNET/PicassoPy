3007195a7bd8c637bf9e5e20bddc316eac9fb40a
Baymax <test@email.com>
2024-06-11 11:16:41 +0000

    added arg-parsing and concat level0 files to single-day-file

--- tests/picassopy_test_rsd2.py
+++ tests/picassopy_test_rsd2.py
@@ -1,40 +1,83 @@
 #from pathlib import Path
 import sys
 import os
+import argparse
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from pathlib import Path
+from log import logger
 #from lib.io.loadConfigs import *
 import lib.io.loadConfigs as loadConfigs
 import lib.io.readPollyRawData as readPollyRawData
 import lib.interface.picassoProc as picassoProc
-from log import logger
 import lib.misc.helper as helper
-## getting root dir of PicassoPy
-root_dir = Path(__file__).resolve().parent.parent
 
-root_dir = helper.detect_path_type(root_dir)
+## getting root dir of PicassoPy
+root_dir0 = Path(__file__).resolve().parent.parent
+root_dir = helper.detect_path_type(root_dir0)
+
 ## setting config files
-picasso_default_config_file = Path(root_dir,'config','pollynet_processing_chain_config.json')
-polly_default_config_file = Path(root_dir,'config','polly_global_config.json')
-picasso_config_file = "/pollyhome/Bildermacher2/experimental/PicassoPy/config/pollynet_processing_chain_config_rsd2_24h_exp.json"
-polly_config_array = loadConfigs.readPollyNetConfigLinkTable(picasso_config_dict['pollynet_config_link_file'],timestamp=20230911,device="pollyxt_lacros")
-polly_config_file = str(polly_config_array['Config file'].to_string(index=False)).strip()
+picasso_default_config_file = Path(root_dir,'lib','config','pollynet_processing_chain_config.json')
+polly_default_config_file = Path(root_dir,'lib','config','polly_global_config.json')
+#picasso_config_file = "/pollyhome/Bildermacher2/experimental/PicassoPy/config/pollynet_processing_chain_config_rsd2_24h_exp.json"
+
+
+my_parser = argparse.ArgumentParser(description='PicassoPy PollynetProcessingChain for Polly devices to process polly level0 data to level1 data.')
+
+## Add the arguments
+my_parser.add_argument('--date', dest='timestamp',
+                       type=str,
+                       help='the date of measurement: YYYYMMDD.')
+my_parser.add_argument('--device',
+                       type=str,
+                       help='the polly device (level1 nc-file).')
+my_parser.add_argument('--base_dir',
+                       type=str,
+                       default='/data/level0/polly',
+                       help='the directory of level0 polly data and logbook-files.')
+my_parser.add_argument('--picasso_config_file',
+                       type=str,
+                       default=picasso_default_config_file,
+                       help='the json-type picasso config-file, default is lib/config/pollynet_processing_chain_config.json')
+my_parser.add_argument('--level0_file_to_process',
+                       type=str,
+                       default=None,
+                       help='specify a level0 polly file to be processed')
+my_parser.add_argument('--merge_to_single_24h_file',
+                       action='store_true',
+                       help='Flag to activate merging of multiple level0 files from one day to a single 24h file.')
+
+# init parser
+args = my_parser.parse_args()
 
 
 ## loading configs as dicts
-picasso_config_dict = loadConfigs.loadPicassoConfig(picasso_config_file,picasso_default_config_file)
-polly_config_file = loadConfigs.readPollyNetConfigLinkTable(picasso_config_dict['pollynet_config_link_file'],timestamp=20230911,device="pollyxt_lacros")
+picasso_config_dict = loadConfigs.loadPicassoConfig(args.picasso_config_file,picasso_default_config_file)
+polly_config_array = loadConfigs.readPollyNetConfigLinkTable(picasso_config_dict['pollynet_config_link_file'],timestamp=args.timestamp,device=args.device)
+polly_config_file = str(polly_config_array['Config file'].to_string(index=False)).strip()
+
+output_path = Path(picasso_config_dict["fileinfo_new"]).parent
+
 if polly_config_file:
     polly_config_file_fullname = Path(picasso_config_dict['polly_config_folder'],polly_config_file)
 else:
     polly_config_file_fullname = polly_default_config_file
 polly_config_dict = loadConfigs.loadPollyConfig(polly_config_file_fullname, polly_default_config_file)
 
+if args.level0_file_to_process != None:
+    rawfile_fullname = args.level0_file_to_process
+elif args.level0_file_to_process == None and args.merge_to_single_24h_file == True:
+    rawfile_fullname = helper.concat_files(timestamp=args.timestamp,device=args.device,raw_folder=args.base_dir,output_path=output_path)
+else:
+    rawfile_fullname = None
+
+
 ## reading level0 polly-nc-file and output as dict
-#rawfile_fullname = 'C:\\_data\\Picasso_IO\\input\\2024_03_20_Wed_ARI_12_00_01.nc'
-rawfile_fullname = 'H:\\picasso_io\\pollyxt_cpv\\data_zip\\202403\\2024_03_08_Fri_CPV_00_00_01.nc'
-rawfile = helper.detect_path_type(rawfile_fullname)
-#rawfile = '/pollyhome/Bildermacher2/experimental/2023_09_11_Mon_LACROS_00_00_01.nc'
+if rawfile_fullname:
+    rawfile = helper.detect_path_type(rawfile_fullname)
+else:
+    print('No level0-file specified or merging option is not set. Aborting.')
+    sys.exit(1)
+
 rawdata_dict = readPollyRawData.readPollyRawData(filename=rawfile)
 
 ## initate picasso-object from class PicassoProc
