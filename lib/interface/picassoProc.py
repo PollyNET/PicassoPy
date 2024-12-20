some fixes to the rcs calculation to be in line with the matlab results
closing also #7

lib/interface/picassoProc.py
@@ -10,6 +10,7 @@ import lib.qc.pollySaturationDetect as pollySaturationDetect
 import lib.calibration.polarization as polarization
 import lib.io.readMeteo as readMeteo
 import lib.misc.molecular as molecular
+import lib.calibration.rayleighfit as rayleighfit
 
 class PicassoProc:
     counter = 0
@@ -185,7 +186,7 @@ class PicassoProc:
 
         return self
 
-    def preprocessing(self):
+    def preprocessing(self, collect_debug=False):
         preproc_dict = pollyPreprocess.pollyPreprocess(self.rawdata_dict,
                 deltaT=self.polly_config_dict['deltaT'],
                 flagForceMeasTime = self.polly_config_dict['flagForceMeasTime'],
@@ -221,6 +222,7 @@ class PicassoProc:
                 flag532nmRotRaman = np.bitwise_and(np.array(self.polly_config_dict['is532nm']), np.array(self.polly_config_dict['isRR'])).tolist(),
                 flag1064nmRotRaman = np.bitwise_and(np.array(self.polly_config_dict['is1064nm']), np.array(self.polly_config_dict['isRR'])).tolist(),
                 isUseLatestGDAS = self.polly_config_dict['flagUseLatestGDAS'],
+                collect_debug=collect_debug,
                 )
         self.data_retrievals.update(preproc_dict)
 
@@ -292,6 +294,13 @@ class PicassoProc:
         print('time slices of cloud free ', time_slices)
         mean_profiles = self.met.get_mean_profiles(time_slices) 
         self.mol_profiles = molecular.calc_profiles(mean_profiles)
+    
+    def rayleighFit(self):
+
+        print('Start Rayleigh Fit')
+
+        return rayleighfit.rayleighfit(self)
+
 
 
 

lib/io/loadConfigs.py
@@ -1,6 +1,7 @@
 from . import *
 import json
 import pandas as pd
+import numpy as np
 
 def loadPicassoConfig(picasso_config_file, picasso_default_config_file):
     picasso_default_config_file_path = Path(picasso_default_config_file)
@@ -70,6 +71,25 @@ def readPollyNetConfigLinkTable(polly_config_table_file, timestamp, device):
         return pd.DataFrame()
 
 
+def fix_indexing(config_dict, keys=['first_range_gate_indx',]):
+    """
+    """
+
+    if not 'indexing_convention' in config_dict:
+        logging.warning(f'indexing_convention not given, assuming 1based')
+        config_dict['indexing_convention'] = '1based'
+
+    for k in keys:
+        print('before', config_dict[k])
+        if isinstance(config_dict[k], list):
+            config_dict[k] = (np.array(config_dict[k])-1).tolist()
+        else:
+            config_dict[k] = config_dict[k] - 1
+        print('after', config_dict[k])
+            
+    return config_dict
+
+
 def loadPollyConfig(polly_config_file, polly_default_config_file):
     polly_default_config_file_path = Path(polly_default_config_file)
     polly_config_file_path = Path(polly_config_file)
@@ -101,14 +121,14 @@ def loadPollyConfig(polly_config_file, polly_default_config_file):
                 for key in polly_config_file_dict.keys():
                     if key not in polly_default_config_file_dict.keys():
                         polly_config_dict[key] = polly_config_file_dict[key]
-                return polly_config_dict
+                return fix_indexing(polly_config_dict)
             except Exception:
                 logging.warning('polly_default_config_file: {polly_default_config_file} can not be read.')
 
         else:
             logging.warning(f'polly_config_file: {polly_config_file} does not exist')
             logging.warning(f'polly_default_config_file: {polly_default_config_file} will be used')
-            return polly_default_config_file_dict
+            return fix_indexing(polly_default_config_file_dict)
     else:
         logging.critical(f'polly_default_config_file:  {polly_default_config_file} can not be found. Aborting')
         return None

lib/preprocess/pollyPreprocess.py
@@ -142,12 +142,18 @@ def pollyRemoveBG(rawSignal,**varargin):
 
     #Replicate the mean matrix along the second dimension
     bg = np.tile(mean_matrix, (1, maxHeightBin, 1))
-
-    signal_out = np.full((rawSignal.shape[0], maxHeightBin, rawSignal.shape[2]), np.nan)
-    for iCh in range(0, rawSignal.shape[2]):
-        signal_out[:, :, iCh] = rawSignal[:,firstBinIndex[iCh]:(maxHeightBin+firstBinIndex[iCh]),iCh] - bg[:, :, iCh]
-
+    signal_out = slicerange(rawSignal, maxHeightBin, firstBinIndex) - bg
     return signal_out, bg
+
+
+def slicerange(array, maxHeightBin, firstBinIndex):
+    """slice a give array from firstBinIndex to maxHeightBin + firstBinIndex"""
+    out = np.full((array.shape[0], maxHeightBin, array.shape[2]), np.nan)
+    
+    for iCh in range(0, array.shape[2]):
+        out[:, :, iCh] = array[:,firstBinIndex[iCh]:(maxHeightBin+firstBinIndex[iCh]),iCh]
+    return out
+
 
 def pollyPolCaliTime(depCalAng, mTime, init_depAng, maskDepCalAng):
 
@@ -220,37 +226,37 @@ def pollyPolCaliTime(depCalAng, mTime, init_depAng, maskDepCalAng):
 
     return depCal_P_Ang_time_start, depCal_P_Ang_time_end, depCal_N_Ang_time_start, depCal_N_Ang_time_end, maskDepCal
 
-def calculate_rcs(datasignal,data_dict,mShots,hRes):
+def calculate_rcs(datasignal,ranges):
     """
     Function for calculating RCS.
 
     Args:
-        data: An object or dictionary containing the fields:
-              - datasignal: 3D NumPy array
-              - data_dict: dict containing infos for time, height, ...
-              - mShots: 2D NumPy array
-              - hRes: Scalar resolution (int or float)
-        channel: Index or condition to slice the first dimension of `signal` and `mShots`.
+        datasignal: 
+            signal to range correct
+        ranges: 
+            ranges that are squared
 
     Returns:
         np.ndarray: Computed RCS array.
     """
 
-    mShots_transposed = mShots.T 
-    mShots_broadcasted = np.expand_dims(mShots, axis=1)
-    
-    height_squared = data_dict['height'] ** 2
-    height_squared_broadcasted = height_squared.reshape(1, -1, 1)
+    print(datasignal.shape)
+    ranges_squared = ranges**2
+    ranges2d = np.repeat(ranges_squared[np.newaxis,:], datasignal.shape[0], axis=0)
+    print('ranges2d', ranges2d.shape)
     
     # Perform the computation
+    #RCS = (
+    #    datasignal / mShots_broadcasted * 150 / float(hRes) * height_squared_broadcasted
+    #)
     RCS = (
-        datasignal / mShots_broadcasted * 150 / float(hRes) * height_squared_broadcasted
+        datasignal * ranges2d[:,:,np.newaxis]
     )
     
     return RCS
 
 
-def pollyPreprocess(rawdata_dict, **param):
+def pollyPreprocess(rawdata_dict, collect_debug=False, **param):
     """
     POLLYPREPROCESS Deadtime correction, background correction, first-bin shift, mask for low-SNR and mask for depolarization-calibration process.
     
@@ -520,7 +526,10 @@ def pollyPreprocess(rawdata_dict, **param):
             deadtime = rawdata_dict['deadtime_polynomial']['var_data']
     )
     data_dict['PCR_cor'] = PCR_Cor
-    data_dict['preproSignal'] = preproSignal 
+    data_dict['PCR_slice'] = slicerange(PCR_Cor, maxHeightBin, firstBinIndex)
+    # most likely the preprocesssed deadtime corrected signal can be omitted
+    if not collect_debug:
+        data_dict['preproSignal'] = preproSignal 
 
     ## Background Substraction
     sigBGCor, bg =  pollyRemoveBG(rawSignal = preproSignal,
@@ -536,6 +545,8 @@ def pollyPreprocess(rawdata_dict, **param):
     logging.info('... height bin calculations')
     # TODO first bin hight might change for different telescopes...
     data_dict['height'] = np.arange(0, sigBGCor.shape[1]) * hRes * np.cos(zenithAng*np.pi/180) + firstBinHeight
+
+    data_dict['range'] = np.arange(0, sigBGCor.shape[1]) * hRes + firstBinHeight
     data_dict['alt'] = data_dict['height'] + float(asl) ## geopotential height
     data_dict['time'] = mTime_unixtimestamp
     data_dict['time64'] = np.array([np.datetime64(t) for t in mTime_obj])
@@ -579,9 +590,10 @@ def pollyPreprocess(rawdata_dict, **param):
     ## Range-corrected Signal calculation
     logging.info('... calculate range-corrected Signal')
     mask = data_dict['lowSNRMask'].mask
-    RCS_masked = np.ma.masked_array(sigBGCor,mask=mask)
+    # masked arry might be slow
+    #RCS_masked = np.ma.masked_array(sigBGCor+bg,mask=mask)
 #    data_dict['RCS'] = calculate_rcs(datasignal=preproSignal,data_dict=data_dict,mShots=mShots,hRes=hRes)
-    data_dict['RCS'] = calculate_rcs(datasignal=RCS_masked,data_dict=data_dict,mShots=mShots,hRes=hRes)
+    data_dict['RCS'] = calculate_rcs(data_dict['PCR_slice'], data_dict['range'])
 
 
     logging.info('finished data preprocessing.')