846e50934a986c464ac9bfcaacb3896e10f241f4
HavardStridBuholdt <havard.strid.buholdt@gmail.com>
2026-03-18 17:15:59 +0100

    Update to Lidar constant calculations. - changed beta_mol for inelastic wavelengths - added 'flagUseRetrievedExtForLC' config variable

--- ppcpy/calibration/lidarconstant.py
+++ ppcpy/calibration/lidarconstant.py
@@ -9,7 +9,7 @@ from ppcpy.misc.helper import mean_stable
 
 elastic2raman = {355: 387, 532: 607}
 
-def lc_for_cldFreeGrps(data_cube, retrieval:str) -> list:
+def lc_for_cldFreeGrps(data_cube, retrieval:str, collect_debug=False) -> list:
     """Estimate the lidar constant from the optical profiles.
 
     Parameters
@@ -18,6 +18,8 @@ def lc_for_cldFreeGrps(data_cube, retrieval:str) -> list:
         Main PicassoProc object.
     retrieval : str
         Retrieval type. 'klett' or 'raman'.
+    collect_debug : bool
+        If true, collects debug information.
     
     Returns
     -------
@@ -27,11 +29,21 @@ def lc_for_cldFreeGrps(data_cube, retrieval:str) -> list:
     Notes
     -----
     - For NR, done directly form the optical profiles, whereas in the matlab version, the ``LC*olAttri387.sigRatio`` is taken.
+    - Through the config variable 'flagUseRetrievedExtForLC', the extinction used to calculate the LCs can be specified. if 
+      'flagUseRetrievedExtForLC' is True the retrieved extinction will be used otherwise the extinction approximated by
+      the backscatter times the assumed lidar constant will be used. In the matlab version the retrieved extinction is used 
+      for the ... and the approximated extinction is used for ....
+    - Missing Rotational Raman and Aeronet LC retrieval.
 
     .. TODO:: Change back to Picasso version to check if lidar calibration constants get more similar.
     .. TODO:: Check if LC's are normalized with respect to the mean of the profiles.
 
+    **History**
+
+    xxxx-xx-xx: First edition by ...
+    2026-03-18: Changed beta_mol for inelastic wavelengths and added the 'flagUseRetrievedExtForLC' variable.
     """
+
     print("retival", retrieval)
     height = data_cube.retrievals_highres['range']
     hres = data_cube.rawdata_dict['measurement_height_resolution']['var_data']
@@ -52,14 +64,16 @@ def lc_for_cldFreeGrps(data_cube, retrieval:str) -> list:
             # Telescope type dependent configurations:
             if tel == 'NR':
                 key_smooth = f'smoothWin_{retrieval}_NR_'
+                key_LR = 'LR_NR_'
             else:
                 key_smooth = f'smoothWin_{retrieval}_'
+                key_LR = 'LR'
 
             hFullOverlap = heightFullOverlap[i][data_cube.gf(wv, t, tel)][0]
             hBaseInd = np.argmax(
                 height >= (hFullOverlap + config_dict[f'{key_smooth}{wv}'] / 2 * hres))
 
-            # Elastic signals:
+            # Elastic signal:
             sig = profiles[channel]['signal']
             signal = np.nanmean(np.squeeze(
                 data_cube.retrievals_highres[f'sig{sig}'][slice(*cldFree), :, data_cube.gf(wv, t, tel)]), axis=0)
@@ -71,9 +85,25 @@ def lc_for_cldFreeGrps(data_cube, retrieval:str) -> list:
                 print(f'No availabel retrievals, skipping {channel} {cldFree}')
                 continue
 
-            aerExt = profiles[channel]['aerExt'].copy()
-            aerExt[:hBaseInd] = aerExt[hBaseInd]
+            # Backscatter and extinction retrievals:
             aerBsc = profiles[channel]['aerBsc']
+            if config_dict['flagUseRetrievedExtForLC']:
+                aerExt = profiles[channel]['aerExt'].copy()
+                aerExt[:hBaseInd + 1] = aerExt[hBaseInd]
+            else:
+                aerBsc[aerBsc < 0] = np.nan
+                aerExt = aerBsc * config_dict[f'{key_LR}{wv}']
+            
+            ##### Picasso comparison #####
+            if config_dict['flagPicassoComparison']:
+                if 'klett' in retrieval:
+                    aerExt = profiles[channel]['aerExt']
+                    aerExt[:hBaseInd + 1] = aerExt[hBaseInd]
+                elif 'raman' in retrieval:
+                    aerBsc[aerBsc <= 0] = np.nan
+                    aerExt = aerBsc * config_dict[f'{key_LR}{wv}']
+                else:
+                    raise ValueError(f'Unknown retrieval type: {retrieval}')
 
             aerOD = np.nancumsum(aerExt * np.concatenate(([height[0]], np.diff(height))))
             molOD = np.nancumsum(molExt * np.concatenate(([height[0]], np.diff(height))))
@@ -81,7 +111,7 @@ def lc_for_cldFreeGrps(data_cube, retrieval:str) -> list:
             trans = np.exp(-2 * (aerOD + molOD))
             bsc = molBsc + aerBsc
 
-            LC = signal * height**2 / bsc / trans
+            LC = (signal * height**2) / (bsc * trans)
             LC_stable, _, LCStd = mean_stable(
                 x=LC,
                 win=config_dict['LCMeanWindow'], 
@@ -91,27 +121,33 @@ def lc_for_cldFreeGrps(data_cube, retrieval:str) -> list:
             print(f"cldFreGrp {i}, Channel {wv} {t} {tel}, LC_stable {LC_stable}, LCStd {LCStd}")
 
             if LC_stable is None:
-                print(f'Can not find a stable LC value, skipping {channel} {cldFree}')
+                print(f'Can not find a stable LC value, skipping {wv}nm {t} {tel} channel for cloud free period {cldFree}')
                 continue
 
-            LCs[i][channel] = {'LC': LC_stable, 'LCStd': LC_stable * LCStd}
+            if collect_debug:
+                LCs[i][channel] = {'LC': LC_stable, 'LCStd': LC_stable * LCStd, 'LC_profile': LC}
+            else:
+                LCs[i][channel] = {'LC': LC_stable, 'LCStd': LC_stable * LCStd}
 
+            # -----------------------------------------------------------------------------------
+            # LC for raman / inelastic channels
+            # -----------------------------------------------------------------------------------
             if retrieval == 'raman' and int(wv) in elastic2raman.keys():
                 wv_r = elastic2raman[int(wv)] 
 
-                # Inelastic signals:
+                # Inelastic signal, backscatter and extinction:
                 signal_r = np.nanmean(np.squeeze(
                     data_cube.retrievals_highres[f'sig{sig}'][slice(*cldFree), :, data_cube.gf(wv_r, t, tel)]), axis=0)
-                #molBsc_r = data_cube.mol_profiles[f'mBsc_{wv_r}'][i, :]
+                molBsc_r = data_cube.mol_profiles[f'mBsc_{wv_r}'][i, :]
                 molExt_r = data_cube.mol_profiles[f'mExt_{wv_r}'][i, :]
                 aerExt_r = aerExt * (int(wv)/int(wv_r))**config_dict['angstrexp'] 
                 aerOD_r = np.cumsum(aerExt_r * np.concatenate(([height[0]], np.diff(height))))
                 molOD_r = np.cumsum(molExt_r * np.concatenate(([height[0]], np.diff(height))))
 
                 trans_r = np.exp(- (aerOD + molOD + aerOD_r + molOD_r))
-                bsc = molBsc
+                bsc_r = molBsc_r
 
-                LC_r = signal_r * height**2 / bsc / trans_r
+                LC_r = (signal_r * height**2) / (bsc_r * trans_r)
                 LC_r_stable, _, LCStd_r = mean_stable(
                     x=LC_r,
                     win=config_dict['LCMeanWindow'], 
@@ -121,10 +157,13 @@ def lc_for_cldFreeGrps(data_cube, retrieval:str) -> list:
                 print(f"cldFreGrp {i}, Channel {wv_r} {t} {tel}, LC_stable {LC_r_stable}, LCStd {LCStd_r}")
 
                 if LC_r_stable is None:
-                    print(f'Can not find a stable LC value, skipping {channel} {cldFree}')
+                    print(f'Can not find a stable LC value, skipping {wv_r}nm {t} {tel} channel for cloud free period {cldFree}')
                     continue
 
-                LCs[i][f"{wv_r}_{t}_{tel}"] = {'LC': LC_r_stable, 'LCStd': LC_r_stable * LCStd_r}
+                if collect_debug:
+                    LCs[i][f"{wv_r}_{t}_{tel}"] =  {'LC': LC_r_stable, 'LCStd': LC_r_stable * LCStd_r, 'LC_profile': LC_r}
+                else:
+                    LCs[i][f"{wv_r}_{t}_{tel}"] =  {'LC': LC_r_stable, 'LCStd': LC_r_stable * LCStd_r}
 
     return LCs
 
@@ -144,23 +183,23 @@ def get_best_LC(LCs:list) -> dict:
 
     Notes
     -----
-    - Since ``LC = LC_stable`` and ``LCStd = LC_stable * LC_Std`` so will any negative LC also have
-      a negative LCStd, and thus be chosen as the best LC.
 
     **History**
 
     - 2026-02-16: Added additional checks to hinder negative LCs to be chosen.
-        
+    - 2026-03-18: Now selects LC based on minimum relative standard deviation.
     """
+
     # list comprehension for nested list
     all_channels = set([k for e in LCs for k in e.keys()])
     
     LCused = {}
     for channel in all_channels:
         lcs = np.array([e[channel]['LC'] for e in LCs if channel in e and e[channel]['LC'] >= 0])
-        lcsstd = np.array([e[channel]['LCStd'] for e in LCs if channel in e and e[channel]['LCStd'] >= 0])
+        lcsstd = np.array([e[channel]['LCStd'] for e in LCs if channel in e and e[channel]['LC'] >= 0])
 
-        LCused[channel] = lcs[np.argmin(lcsstd)]
+        LCused[channel] = lcs[np.argmin(lcsstd / lcs)]
+    
     return LCused
 
 
