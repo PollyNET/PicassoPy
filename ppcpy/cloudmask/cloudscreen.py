8a7bc52df1d5b8f7d508468de983631154c95774
HavardStridBuholdt <havard.strid.buholdt@gmail.com>
2026-04-09 18:48:51 +0200

    Enhanced robustness against NaN values. Added debugging options. Added "maxCloudSearchHeight" as config parameter.

--- ppcpy/cloudmask/cloudscreen.py
+++ ppcpy/cloudmask/cloudscreen.py
@@ -1,6 +1,7 @@
 import numpy as np
 from scipy.ndimage import label, uniform_filter1d
 from ppcpy.misc.helper import uniform_filter, savgol_filter
+import matplotlib.pyplot as plt
 
 
 def smooth_signal(signal:np.ndarray, window_len:int) -> np.ndarray:
@@ -32,7 +33,7 @@ def smooth_signal(signal:np.ndarray, window_len:int) -> np.ndarray:
     return uniform_filter1d(signal, window_len, mode='nearest')
 
 
-def cloudscreen(data_cube, wv=532) -> np.ndarray:
+def cloudscreen(data_cube, wv:int=532, collect_debug:bool=False) -> np.ndarray:
     """Preform cloud screening.
 
     Parameters
@@ -41,6 +42,8 @@ def cloudscreen(data_cube, wv=532) -> np.ndarray:
         Main PicassoProc object.
     wv : int, optional
         Wavelength of the channel to perfom cloud screening [nm]. Default is 532.
+    collect_debug : bool, optional
+        If true, collects debug information. Default is False.
     
     Returns
     -------
@@ -56,30 +59,32 @@ def cloudscreen(data_cube, wv=532) -> np.ndarray:
     
     - xxxx-xx-xx: First edition by ...
     - 2026-03-18: Updated to include necessary configurations for cloudScreen_Zhao.
+    - 2026-04-09: Added 'maxCloudSearchHeight' config parameters.
     """
 
-    config_dict = data_cube.polly_config_dict
     print('Starting cloud screen')
-    print('cloud screen mode', config_dict['cloudScreenMode'])
-
+    config_dict = data_cube.polly_config_dict
     height = data_cube.retrievals_highres['range']
     bg = np.squeeze(data_cube.retrievals_highres['BG'][:, data_cube.gf(wv, 'total', 'FR')])
     hFullOL = np.array(config_dict['heightFullOverlap'])[data_cube.gf(wv, 'total', 'FR')][0]
 
     # Cloud screen mode dependent configuration
     if config_dict['cloudScreenMode'] == 1:
+        print(f'cloud screen mode {config_dict['cloudScreenMode']}: MSG method')
         screenfunc = cloudScreen_MSG
         sig = 'RCS'
         kwargs = {
             'slope_thres': config_dict['maxSigSlope4FilterCloud'],
         }
     elif config_dict['cloudScreenMode'] == 2:
+        print(f"cloud screen mode {config_dict['cloudScreenMode']}: Zhao's method")
         screenfunc = cloudScreen_Zhao
         sig = 'PCR_slice'
         kwargs = {
             'bg': bg,
             'minSNR': 2, 
-            'heightFullOverlap': hFullOL
+            'heightFullOverlap': hFullOL,
+            'showDetails': collect_debug
         }
     else:
         raise ValueError(f'cloudScreenMode {config_dict['cloudScreenMode']} not properly defined.')
@@ -88,7 +93,7 @@ def cloudscreen(data_cube, wv=532) -> np.ndarray:
 
     flagCloudFree, layerStatus = screenfunc(
         height=height, signal=signal,
-        search_region=[hFullOL, 7000],
+        search_region=[hFullOL, config_dict['maxCloudSearchHeight']],
         **kwargs
     )
 
@@ -104,7 +109,7 @@ def cloudscreen(data_cube, wv=532) -> np.ndarray:
 
         flagCloudFree_NR, layerStatus_NR = screenfunc(
             height=height, signal=signal,
-            search_region=[hFullOL, 2000],
+            search_region=[hFullOL, config_dict['maxCloudSearchHeight_NR']],
             **kwargs
         )
 
@@ -160,8 +165,8 @@ def cloudScreen_MSG(height:np.ndarray, signal:np.ndarray, slope_thres:float, sea
     search_index = np.searchsorted(height, np.array(search_region))
 
     for indx in range(signal.shape[0]):
-        if np.isnan(signal[indx, 0]):
-            print(f"Skipping timestamp {indx}.")
+        if np.isnan(signal[indx]).all():
+            print(f'Skipping timestamp {indx}')
             continue
 
         slope = np.concatenate(([0], np.diff(smooth_signal(signal[indx, :], smooth_win)))) / (height[1] - height[0])
@@ -173,7 +178,8 @@ def cloudScreen_MSG(height:np.ndarray, signal:np.ndarray, slope_thres:float, sea
 
 
 def cloudScreen_Zhao(height:np.ndarray, signal:np.ndarray, bg:np.ndarray, search_region:list=[0, 10000],
-                     minDepth:float=100, heightFullOverlap:float=600, smoothWin:int=7, minSNR:float=1) -> tuple:
+                     minDepth:float=100, heightFullOverlap:float=600, smoothWin:int=7, minSNR:float=1,
+                     showDetails:bool=False) -> tuple:
     """Cloud layer detection based on Zhao's algorithm.
 
     Parameters
@@ -225,6 +231,13 @@ def cloudScreen_Zhao(height:np.ndarray, signal:np.ndarray, bg:np.ndarray, search
     flagDetectBins = (height >= search_region[0]) & (height <= search_region[1])
 
     for iTime in range(signal.shape[0]):
+        if showDetails:
+            print(f"--------- iTime: {iTime} ---------")
+
+        if np.isnan(signal[iTime, flagDetectBins]).all():
+            print(f'Skipping timestamp {iTime}')
+            continue
+
         ## layer detection
         layerInfo, _, _ = VDE_cld(
             signal=signal[iTime, flagDetectBins],
@@ -233,7 +246,8 @@ def cloudScreen_Zhao(height:np.ndarray, signal:np.ndarray, bg:np.ndarray, search
             minLayerDepth=minDepth / 1e3,
             minHeight=heightFullOverlap / 1e3,
             smoothWin=smoothWin,
-            minSNR=minSNR
+            minSNR=minSNR,
+            showDetails=showDetails
         )
 
         for iLayer in range(len(layerInfo)):
@@ -254,7 +268,8 @@ def cloudScreen_Zhao(height:np.ndarray, signal:np.ndarray, bg:np.ndarray, search
 
 
 def VDE_cld(signal:np.ndarray, height:np.ndarray, BG:float, minLayerDepth:float=0.2,
-            minHeight:float=0.4, smoothWin:int=3, savgolSmoothWin:int=9, minSNR:int=5) -> tuple:
+            minHeight:float=0.4, smoothWin:int=3, savgolSmoothWin:int=9, minSNR:int=5,
+            showDetails:bool=False) -> tuple:
     """Cloud layer detection with VDE method. This method only required elstic signal.
     
     Parameters
@@ -311,8 +326,9 @@ def VDE_cld(signal:np.ndarray, height:np.ndarray, BG:float, minLayerDepth:float=
 
     **History**
     
-    - 2021-06-13: First edition by Zhenping
-    - 2026-03-11: Translated into pyhton
+    - 2021-06-13: First edition by Zhenping.
+    - 2026-03-11: Translated into pyhton.
+    - 2026-04-09: Enhanced robustness against NaN values.
     """
 
     if np.floor(minLayerDepth / (height[1] - height[0])) < 3:
@@ -328,7 +344,9 @@ def VDE_cld(signal:np.ndarray, height:np.ndarray, BG:float, minLayerDepth:float=
     # ----------------------------------------------------
     # 1. Semi-Discretization Process (SDP)
     # ----------------------------------------------------
-    noise_tresh = np.maximum(np.sqrt(BG + P)*3, np.sqrt(3)*3)
+    noise = np.sqrt(P + BG)*3
+    min_noise = np.ones_like(P)*np.sqrt(3)*3
+    noise_tresh = np.nanmax(np.vstack([noise, min_noise]), axis=0)
     Ps = smooth_signal(P, smoothWin)
 
     ## bottom to top semi-discretization
@@ -348,14 +366,14 @@ def VDE_cld(signal:np.ndarray, height:np.ndarray, BG:float, minLayerDepth:float=
     # ----------------------------------------------------
     # 2. Value Distribution Equalization (VDE) Process
     # ----------------------------------------------------
-    PD = PD[~np.isnan(PD)]
+    # PD = PD[~np.isnan(PD)]
     IS = np.argsort(PD)
     RS = PD[IS]
 
-    MA = RS[-1]
-    MI = RS[0]
+    MA = np.nanmax(RS)
+    MI = np.nanmin(RS)
 
-    PE = np.arange(len(RS)) / len(RS)
+    PE = np.arange(1, len(RS)+1) / len(RS)
     epsilon = 1e-6
 
     for i in range(1, len(RS)):
@@ -374,6 +392,19 @@ def VDE_cld(signal:np.ndarray, height:np.ndarray, BG:float, minLayerDepth:float=
     layerN = 0
     L, nLayer = label(PN > BZ)
 
+    if showDetails:
+        fig, ax = plt.subplots(figsize=(12, 6))
+        ax.plot(height, P, color="black", alpha=0.3, label="P(R)")
+        ax.plot(height, Ps, color="black", linestyle="--", alpha=0.3, label="Ps(R)")
+        # ax.plot(height, PD, color="tab:green", alpha=0.7, label='PD')
+        ax.plot(height, PN, color="tab:blue", alpha=0.7, label='PN(R)')
+        ax.plot(height, BZ, color="tab:orange", alpha=0.7,  label='BZ(R)')
+        ax.set_xlabel("Range [km]")
+        ax.set_ylabel("Photon count")
+        ax.legend()
+        fig.tight_layout()
+        plt.show()
+
     for iLayer in range(1, nLayer + 1):
 
         baseIndex = np.where(L == iLayer)[0][0]
@@ -383,6 +414,9 @@ def VDE_cld(signal:np.ndarray, height:np.ndarray, BG:float, minLayerDepth:float=
         layerIndex = (L == iLayer)
         layerSNR = np.nanmean(P[layerIndex]) / np.sqrt(np.nanmean(P[layerIndex] + BG))
 
+        if showDetails:
+            print(f"layer: {iLayer} at height [{height[baseIndex]:.3f}, {height[topIndex]:.3f}] km, Depth: {layerDepth:.3f} >= {minLayerDepth}, SNR: {layerSNR:.3f} >= {minSNR}")
+        
         if (layerDepth >= minLayerDepth) and (layerSNR >= minSNR):
 
             layerN += 1
@@ -401,7 +435,8 @@ def VDE_cld(signal:np.ndarray, height:np.ndarray, BG:float, minLayerDepth:float=
     for iLayer in range(len(layerInfo)):
         mask = (height >= layerInfo[iLayer]['baseHeight']) & (height <= layerInfo[iLayer]['topHeight'])
         sig = Ps_1[mask] * height[mask]**2
-        sig[sig <= 0] = np.nan
+        sig[sig <= 0] = np.nan  # may cause T and or D to be NaN, thus affecting the determined layer satus
+        # sig[sig <= 0] = epsilon 
 
         maxIndex = np.nanargmax(sig) # potential crash here if all of sig = NaN
         maxSig = sig[maxIndex]
@@ -416,13 +451,19 @@ def VDE_cld(signal:np.ndarray, height:np.ndarray, BG:float, minLayerDepth:float=
         if layerHeight <= 1.5:
             if T > 6 or D < -15:
                 layerInfo[iLayer]['flagCloud'] = True
+
         elif layerHeight > 1.5 and layerHeight <= 5:
             if T > 4 or D < -9:
                 layerInfo[iLayer]['flagCloud'] = True
+
         elif layerHeight > 5:
             if T > 1.5 or D < -9:
                 layerInfo[iLayer]['flagCloud'] = True
+                
         else:
             raise ValueError('Invalid height!')
+        
+        if showDetails and layerInfo[iLayer]['flagCloud']:
+            print(f"Cloud detected at height: {layerHeight:.3f} km T = {T:.1f}, D = {D:.1f}")
 
     return layerInfo, PD, PN
